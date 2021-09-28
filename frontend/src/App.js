5ee25f7676a1c24d031f538db0df6545dddef0bb
Arthur Rodrigues <arthurginho02@gmail.com>
2021-09-28 19:18:48 -0300

    adiconado cartReducers cartActions e cartComponents

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,31 @@
-import { BrowserRouter, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
 import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
 import CartPage from './Pages/CartPage';
 import HomePage from './Pages/HomePage';
 import ProductPage from './Pages/ProductPage';
 
 function App() {
+  
+  const cart = useSelector(state => state.cart)
+  const {cartItems} = cart;
+
   return (
     <BrowserRouter> 
      <div className="grid-container">
       <header className="row">
         <div>
-          <a className="brand" href="/">
+          <Link className="brand" to="/">
             E-commerce
-          </a>
+          </Link>
         </div>
         <div>
-          <a href="/cart">Cart</a>
-          <a href="/signin">Sign In</a>
+          <Link to="/cart">Cart
+          {cartItems.length > 0 && (
+            <span className="badge">{cartItems.length}</span>
+          )}
+          </Link>
+          <Link to="/signin">Sign In</Link>
         </div>
       </header>
       <main>
